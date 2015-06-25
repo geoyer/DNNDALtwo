c7f4a34e7e8a4a73cc3fec1637d2187f3bd1d4d5
hungrywolf418 <brycejackson1129@gmail.com>
2015-06-25 21:27:02 +0100

    Initial Commit

---
+++ Components/Item.cs
@@ -0,0 +1,54 @@
+﻿/*
+' Copyright (c) 2015 Christoc.com
+'  All rights reserved.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+' TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+' THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+' DEALINGS IN THE SOFTWARE.
+' 
+*/
+
+using System;
+using System.Web.Caching;
+using DotNetNuke.Common.Utilities;
+using DotNetNuke.ComponentModel.DataAnnotations;
+using DotNetNuke.Entities.Content;
+
+namespace Christoc.Modules.DNNDAL2.Components
+{
+    [TableName("city")]
+    //setup the primary key for table
+    [PrimaryKey("ID", AutoIncrement = true)]
+    //configure caching using PetaPoco
+    [Cacheable("Items", CacheItemPriority.Default, 20)]
+    //scope the objects to the ModuleId of a module on a page (or copy of a module on a page)
+    [Scope("ModuleId")]
+    class Item
+    {
+        ///<summary>
+        /// The ID of your object with the name of the ItemName
+        ///</summary>
+        public int ID { get; set; }
+        ///<summary>
+        /// A string with the name of the ItemName
+        ///</summary>
+        public string Name { get; set; }
+
+        ///<summary>
+        /// A string with the description of the object
+        ///</summary>
+        public string CountryCode { get; set; }
+
+        ///<summary>
+        /// An integer with the user id of the assigned user for the object
+        ///</summary>
+        public string District { get; set; }
+
+        ///<summary>
+        /// The ModuleId of where the object was created and gets displayed
+        ///</summary>
+        public int Population { get; set; }
+    }
+}
---
+++ Components/ItemController.cs
@@ -0,0 +1,106 @@
+﻿/*
+' Copyright (c) 2015 Christoc.com
+'  All rights reserved.
+' 
+' THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+' TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+' THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+' CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+' DEALINGS IN THE SOFTWARE.
+' 
+*/
+using System.Collections.Generic;
+using DotNetNuke.Data;
+
+
+//This Must Be added
+using System.Linq;
+
+
+namespace Christoc.Modules.DNNDAL2.Components
+{
+    class ItemController
+    {
+        public void CreateItem(Item t)
+        {
+            using (IDataContext ctx = DataContext.Instance())
+            {
+                var rep = ctx.GetRepository<Item>();
+                rep.Insert(t);
+            }
+        }
+
+        public void DeleteItem(int itemId, int moduleId)
+        {
+            var t = GetItem(itemId, moduleId);
+            DeleteItem(t);
+        }
+
+        public void DeleteItem(Item t)
+        {
+            using (IDataContext ctx = DataContext.Instance())
+            {
+                var rep = ctx.GetRepository<Item>();
+                rep.Delete(t);
+            }
+        }
+
+        public IEnumerable<Item> GetItems(int moduleId)
+        {
+            IEnumerable<Item> t;
+            using (IDataContext ctx = DataContext.Instance())
+            {
+                var rep = ctx.GetRepository<Item>();
+                t = rep.Get(moduleId);
+            }
+            return t;
+        }
+
+        public Item GetItem(int itemId, int moduleId)
+        {
+            Item t;
+            using (IDataContext ctx = DataContext.Instance())
+            {
+                var rep = ctx.GetRepository<Item>();
+                t = rep.GetById(itemId, moduleId);
+            }
+            return t;
+        }
+
+        public void UpdateItem(Item t)
+        {
+            using (IDataContext ctx = DataContext.Instance())
+            {
+                var rep = ctx.GetRepository<Item>();
+                rep.Update(t);
+            }
+        }
+
+
+        //Andrew's Additions
+
+        public IEnumerable<Item> GetCountries()
+        {
+            IEnumerable<Item> t;
+            using (IDataContext ctx = DataContext.Instance())
+            {
+
+                //VERSION 1 - Base Implementation
+                //var rep = ctx.GetRepository<Item>();
+                //t = rep.Get();
+
+
+                //VERSION 2 - LINQ Controls / Sorting - Added By AB
+                var sorted = ctx.GetRepository<Item>();
+
+                //t = sorted.Find("WHERE ID > 0").OrderBy(x => x.Name);
+                t = sorted.Get().OrderBy(x => x.Name);
+
+                
+
+            }
+            return t;
+        }
+
+    }
+}
